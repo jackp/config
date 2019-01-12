5383d63b07a907d6e05dffe27ffc3febd497cc44
Jackson Parker <jack@rethinq.io>
2019-01-12 09:44:22 -0500

    Init

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# Configure osx settings
+./.macos
+
+# Install Home-brew
+# https://brew.sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install Home-brew dependences from ~/Brewfile
+brew bundle
+
+# TODO: Alfred configuration
+# Preferences
+# Change default spotlight hotkey
+# Activate powerpack
